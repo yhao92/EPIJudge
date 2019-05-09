c104a3946e515085fd1ee92d841522b0f387d05f
Yuhao Wu <yhao.wu@gmail.com>
2019-05-08 21:17:48 -0700

    Add Java solution for 17.4

--- epi_judge_java/epi/BinomialCoefficients.java
+++ epi_judge_java/epi/BinomialCoefficients.java
@@ -5,8 +5,19 @@ public class BinomialCoefficients {
   @EpiTest(testDataFile = "binomial_coefficients.tsv")
 
   public static int computeBinomialCoefficient(int n, int k) {
-    // TODO - you fill in here.
-    return 0;
+    int[][] binomials = new int[n + 1][k + 1];
+    return compute(n, k, binomials);
+  }
+
+  private static int compute(int n, int k, int[][] binomials) {
+    if (k == 0 || n == k)
+      return 1;
+    if (binomials[n][k] == 0) {
+      int without_n = compute(n - 1, k, binomials);
+      int with_n = compute(n - 1, k - 1, binomials);
+      binomials[n][k] = without_n + with_n;
+    }
+    return binomials[n][k];
   }
 
   public static void main(String[] args) {
